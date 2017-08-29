60efd0c19f1b8ab5a0d8f730d40ab3abce9adf5e
Milen Dyankov <milendyankov@gmail.com>
2017-08-29 21:03:18 +0200

    real integration test

--- src/it/simple-it/verify.groovy
+++ src/it/simple-it/verify.groovy
@@ -1,3 +1,19 @@
-File touchFile = new File( basedir, "target/touch.txt" );
 
-assert touchFile.isFile()
+result = false;
+
+for (i = 0; i <10; i++) {
+	println("attempt " + i)
+	sleep(1000)
+	output = ""
+	try {
+		output = 'https://maven-plugin-test-javaapitest.wedeploy.io/'.toURL().text;
+		println (output)
+	} catch (Exception e) {
+	}	
+	result = output == "It works"
+	if (result) {
+		break;
+	}
+}
+
+assert result;
